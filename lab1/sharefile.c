433eef86d0d96c504d9e06e17c44225b12728b29
Arif Balık <arif.balik@katihal.com>
2019-03-07 01:17:03 +0300

    updated code
    wrong file updated in the last commit

--- lab1/sharefile.c
+++ lab1/sharefile.c
@@ -1,5 +1,11 @@
 /*
-* 
+ * System Programming Course
+ * Lab #1
+ * Concurrent File Acces by Parent and Child Processes
+ * Arif Ahmet Balik - 180303019
+ * Text Editor : Sublime Text 3
+ * Last Update : 7 March 2019
+ * All the files can be found at github.com/arifbalik/SYSTEM_PROG_LAB
 */
 
 #include <stdio.h>
@@ -7,37 +13,50 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/uio.h>
+#include <sys/time.h>
 #include <unistd.h>
 
-#define DEBUG
+#define __FDRD 3
+#define __FDWT 4
+
+/* 
+ * Uncomment to enable debug mode. 
+ * When disabled it saves 3000 microseconds!!!
+ */
+#define DEBUG 
 #define WAIT 10000
+
+#define HELP "Please use the command as follows;\n" \
+			 ".\\sharefile.c [input_file] [output_file]"
 
 int main(int argc, char** argv){
 
-	int fdrd, fdwt;
-	int pid;
-	char parent = 'P', child = 'C', c;
+	#ifdef DEBUG
+		struct timeval  t1, t2;
+		gettimeofday(&t1, NULL);
+	#endif
+
+	char pid, c, error;
 	unsigned long i = WAIT;
 
 	if(argc > 3){
-		printf("Too much parameter! Please use the command as follows;\n .\\sharefile.c [input_file] [output_file] \n");
+		printf("Too much parameter!" HELP "\n");
 		exit(1);
 	}else if(argc < 3){
-		printf("Not enough parameter! Please use the command as follows;\n .\\sharefile.c [input_file] [output_file] \n");
+		printf("Not enough parameter!" HELP "\n");
 		exit(1);
 	}
 
-	if((fdrd = open(argv[1], O_RDONLY)) == -1){
+	if(open(argv[1], O_RDONLY) == -1){
 		perror("Oops!");
 		exit(1);
 	}
-	if((fdwt = creat(argv[2], 0666)) == -1){
+	if(open(argv[2], O_CREAT|O_WRONLY|O_TRUNC, 0666) == -1){
 		perror("Oops!");
 		exit(1);
 	}
 
 	#ifdef DEBUG
-		printf("fdrd: %d, fdwt: %d\n",fdrd,fdwt );
 		printf("Parent: creating child process \n");
 	#endif
 
@@ -48,18 +67,26 @@ int main(int argc, char** argv){
 			printf("Child process starts, id: %d\n", getpid());
 		#endif
 		while(1){
-			if(read(fdrd, &c, 1) != 1){
-				printf("\nChild: end of the text \n");
+			error = read(__FDRD, &c, 1);
+			if(error == -1){
+				perror("Oops!");
 				break;
-			} 
+			} else if(!error){
+				#ifdef DEBUG
+					printf("\nChild: End of the file!\n");
+				#endif
+				break;
+			}
 			while(--i);
 			i = WAIT;
-			write(1,"\nChild read:",12);
-			write(1, &c, 1);
-			write(fdwt, &c, 1);
+			#ifdef DEBUG
+				write(1,"\nChild have read:",17);
+				write(1, &c, 1);
+			#endif
+			if(write(__FDWT, &c, 1) == -1) perror("Oops!");
 		}
 		#ifdef DEBUG
-			printf("Exiting child process\n");
+			printf("\nExiting child process\n");
 		#endif
 		exit(0);
 	}
@@ -70,23 +97,34 @@ int main(int argc, char** argv){
 		#endif
 
 		while(1){
-
-		if(read(fdrd, &c, 1) != 1){
+		error = read(__FDRD, &c, 1);
+		if(error == -1){
+			perror("Oops!");
+			break;
+		} else if(!error){
 			#ifdef DEBUG
-				printf("\nParent: end of the text \n");
+				printf("\nParent: End of the file!\n");
 			#endif
 			break;
-		} 
+		}
 		while(--i);
 		i = WAIT;
-		write(1,"\nParent read:",13);
-		write(1, &c, 1);
-		write(fdwt, &c, 1);
+		#ifdef DEBUG
+			write(1,"\nParent read:",13);
+			write(1, &c, 1);
+		#endif
+		if(write(__FDWT, &c, 1) == -1) perror("Oops!");
 	}
 
 	wait(0);
+	close(__FDRD);
+	close(__FDWT);
+
 	#ifdef DEBUG
-			printf("Exiting parent process\n");
+		gettimeofday(&t2, NULL);
+		printf ("Execution time = %f uSeconds\n", 
+			(double) (t2.tv_usec - t1.tv_usec));
+		printf("Exiting parent process\n");
 	#endif
 		
 	}
