ad841314d23ab3f0a5987aa442478f177ce5d4ca
Arif Balık <arif.balik@katihal.com>
2019-03-21 00:57:16 +0300

    update on part 1 and renaming (lab3)

--- lab3/part1.c
+++ lab3/part1.c
@@ -4,16 +4,24 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <sys/uio.h>
-#include <sys/time.h>
 #include <unistd.h>
 
-#define HELP "./file [file1] [file2] [file3]"
+#define HELP "./part1 [file1] [file2] [file3]"
+
+#define OPEN 1
+#define CREATE 0
+#define OPEN_FILE(path, o_c, fd)	fd = ((o_c) ? open(path, O_RDONLY) : open(path, O_CREAT|O_WRONLY|O_TRUNC, 0666));\
+					if(fd == -1){\
+						perror("Error while file creation/opening");\
+						exit(1);\
+					}
+
+
 
 #define PARENT_READ_BLOCK 10
 #define CHILD_READ_BLOCK 20
 
-#define WAIT 10
+#define WAIT 1000
 
 int read_write(int fd_read, int fd_write, int size){
 	char* block;
@@ -25,7 +33,8 @@ int read_write(int fd_read, int fd_write, int size){
 	bytes = read(fd_read, block, size);
 	switch(bytes){
 		case 0:
-			printf("File 1: EOF\n");
+			printf("%s File 1: EOF\n", 
+				(size == PARENT_READ_BLOCK) ? "Parent" : "Child");
 			goto eof;
 		break;
 		case -1:
@@ -43,7 +52,8 @@ int read_write(int fd_read, int fd_write, int size){
 				return 1; //ready for the next sequence
 			}else if(bytes > 0){ 
 				write(fd_write, block, bytes);
-				printf("File1: EOF with %d bytes\n", bytes);
+				printf("%s File1: EOF with %d bytes\n",
+				(size == PARENT_READ_BLOCK) ? "Parent" : "Child", bytes);
 			}else{
 				printf("Error during read File 1. bytes: %d\n",bytes);
 				goto error;
@@ -64,32 +74,22 @@ int read_write(int fd_read, int fd_write, int size){
 int main(int argc, char** argv)
 {
 	short fd1, fd2, fd3;
+
 	if(argc != 4){
 		printf("Invalid argument count. Help\n"HELP"\n" );
 		exit(1);
 	}
 
-	if((fd1 = open(argv[1], O_RDONLY)) == -1){
-		perror("Error while opening [file1]!");
-		exit(1);
-	}
+	OPEN_FILE(argv[1], OPEN, fd1);
 
 	if(!fork()){
-		if((fd3 = open(argv[3], O_CREAT|O_WRONLY|O_TRUNC, 0666)) == -1){
-			perror("Error while creating [file3]");
-			exit(1);
-		}
-
+		OPEN_FILE(argv[3], CREATE, fd3);
 		while(read_write(fd1, fd3, CHILD_READ_BLOCK));
-		
 		close(fd3);
 		exit(1);
 	}
 
-	if((fd2 = open(argv[2], O_CREAT|O_WRONLY|O_TRUNC, 0666)) == -1){
-		perror("Error while creating [file2]");
-		exit(1);
-	}
+	OPEN_FILE(argv[2], CREATE, fd2);
 	
 	while(read_write(fd1, fd2, PARENT_READ_BLOCK));
 	
