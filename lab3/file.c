19b0d7f7f4f116d45c6a24c398d066094b2d0dc1
Arif Balık <arif.balik@katihal.com>
2019-03-21 00:20:21 +0300

    Update part1 in lab 3

--- lab3/file.c
+++ lab3/file.c
@@ -13,18 +13,59 @@
 #define PARENT_READ_BLOCK 10
 #define CHILD_READ_BLOCK 20
 
-#define WAIT 100000
+#define WAIT 10
+
+int read_write(int fd_read, int fd_write, int size){
+	char* block;
+	int i;
+	short bytes;
+
+	block = (char*)malloc((char)size);
+
+	bytes = read(fd_read, block, size);
+	switch(bytes){
+		case 0:
+			printf("File 1: EOF\n");
+			goto eof;
+		break;
+		case -1:
+			perror("Error during read from File 1");
+			goto error;
+		break;
+		default:
+			if(bytes == size){
+				i = WAIT;
+				while(i--);
+				if(write(fd_write, block, size) != size){
+				perror("Error during write into File 3");
+				goto error;
+				}
+				return 1; //ready for the next sequence
+			}else if(bytes > 0){ 
+				write(fd_write, block, bytes);
+				printf("File1: EOF with %d bytes\n", bytes);
+			}else{
+				printf("Error during read File 1. bytes: %d\n",bytes);
+				goto error;
+			}
+			goto eof;
+		break;
+	}
+
+	error:
+	free(block);
+	exit(1);
+
+	eof:
+	free(block);
+	return 0;
+}
 
 int main(int argc, char** argv)
 {
-	char *block;
-	int i;
-	short bytes, fd1, fd2, fd3;
-
-	
-	
+	short fd1, fd2, fd3;
 	if(argc != 4){
-		printf("Invalid argument count. Please use the following standart\n"HELP"\n" );
+		printf("Invalid argument count. Help\n"HELP"\n" );
 		exit(1);
 	}
 
@@ -38,85 +79,22 @@ int main(int argc, char** argv)
 			perror("Error while creating [file3]");
 			exit(1);
 		}
-		block = (char*)malloc((char)CHILD_READ_BLOCK);
-		while(1){
-			bytes = read(fd1, block, CHILD_READ_BLOCK);
-			switch(bytes){
-				case 0:
-					printf("Child: File 1: EOF\n");
-					close(fd3);
-					goto exit;
-				break;
-				case CHILD_READ_BLOCK:
-					i = WAIT;
-					while(i--);
-					if(write(fd3, block, CHILD_READ_BLOCK) != CHILD_READ_BLOCK){
-						perror("Error during write into File 3");
-						exit(1);
-					}
-				break;
-				case -1:
-					perror("Opps!");
-					goto exit;
-				break;
-				default:
-					if(bytes > 0){ 
-						write(fd3, block, bytes);
-						printf("Child: File1: EOF with %d bytes\n", bytes);
-					}
-					else{
-						printf("Child: Error during read File 1. bytes: %d\n",bytes);
-					}
-					goto exit;
-				break;
-			}
-		}
 
-		goto exit;
+		while(read_write(fd1, fd3, CHILD_READ_BLOCK));
+		
+		close(fd3);
+		exit(1);
 	}
+
 	if((fd2 = open(argv[2], O_CREAT|O_WRONLY|O_TRUNC, 0666)) == -1){
 		perror("Error while creating [file2]");
 		exit(1);
 	}
-	block = (char*)malloc((char)PARENT_READ_BLOCK);
-	while(1){
-		bytes = read(fd1, block, PARENT_READ_BLOCK);
-
-		switch(bytes){
-			case 0:
-				printf("Parent: File 1: EOF\n");
-				close(fd2);
-				wait(0);
-				close(fd1);
-				goto exit;
-			break;
-			case PARENT_READ_BLOCK:
-				i = WAIT;
-				while(i--);
-				if(write(fd2, block, PARENT_READ_BLOCK) != PARENT_READ_BLOCK){
-					perror("Error during write into File 2");
-					goto exit;
-				}
-			break;
-			case -1:
-				perror("Opps!");
-			break;
-			default:
-				if(bytes > 0){
-					write(fd2, block, bytes);
-					printf("Parent: File1: EOF with %d bytes\n", bytes);
-				}
-				else{
-					printf("Parent: Error during read File 1.bytes: %d\n",bytes);
-				}
-				goto exit;
-			break;
-		}
-	}
-
-	exit:
-	free(block);
-	exit(1);
-
-
+	
+	while(read_write(fd1, fd2, PARENT_READ_BLOCK));
+	
+	close(fd2);
+	wait(0);
+	close(fd3);
+	
 }
